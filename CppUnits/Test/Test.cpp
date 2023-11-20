05e1f69e7c796445e5016e7c8b8aebc4f83b8b6d
vadym <vadymua@outlook.com>
2023-11-20 01:35:00 +0100

    README update.

--- CppUnits/Test/Test.cpp
+++ CppUnits/Test/Test.cpp
@@ -9,18 +9,27 @@ void print(const Resources::Float128& number)
 	std::cout << std::fixed << number << std::endl;
 }
 
+void print(const Millimeters_t& number)
+{
+	std::cout << std::fixed << number << std::endl;
+}
+
 int main(void)
 {
 	Millimeters_t millimeters(190001);
-	Centimeters_t centimeters(122.53235);
-	Meters_t meters(14.2473738);
-	Kilometers_t kilometers(6325);
+	Centimeters_t centimeters;
+	centimeters = 24.24;
+	Meters_t meters{ 14.2473738 };
+	Kilometers_t kilometers;
+	kilometers = centimeters;
 
-	//centimeters = millimeters;
+	centimeters = centimeters + 120;
 	meters = centimeters;
 	millimeters = kilometers;
 
+	std::cin >> centimeters;
+
 	print(meters());
-	print(millimeters());
+	print(millimeters);
 
 }
